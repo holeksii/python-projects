e343363623c633440b2ad50e9dae54c2c6a9c085
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2021-09-28 23:42:34 +0300

    Update pract2.py

--- pract2.py
+++ pract2.py
@@ -1,4 +1,3 @@
-# 2
 import numpy as np
 from random import randrange
 from math import ceil
@@ -37,7 +36,7 @@ def getPositiveNumber(m):
         break
     return num
 
-def inputMatrix():
+def enterMatrix():
     print("Enter number of rows: ")
     N = getPositiveNumber(True)
     print("Enter number of columns: ")
@@ -96,82 +95,48 @@ def sortMatrix(matrix):
     print("SORTED MATRIX:\n", matrix)
     return matrix
 
-def arrayBinarySearch (arr, l, r, x):
-    if r >= l:
-        mid = l + (r - l) // 2
-
-        if arr[mid] == x:
-            return mid
-        elif arr[mid] > x:
-            return arrayBinarySearch(arr, l, mid-1, x)
-        else:
-            return arrayBinarySearch(arr, mid + 1, r, x)
-    else:
-        return -1
-
-def MatrixbinarySearch(matrix, srchVal):
-    found = False
-
-    rows = len(matrix)
+def matrixBinarySearch(matrix, srchVal):
     cols = len(matrix[0])
+    rows = len(matrix)
+    lo = 0
+    hi = rows * cols - 1
+    while lo <= hi:
+        mi = lo + ((hi-lo) >> 1)
+        row = mi // cols
+        col = mi % cols
+        if(matrix[row][col] == srchVal): 
+            return row, col
+        elif(matrix[row][col] > srchVal):
+            hi = mi - 1
+            # return matrixBinarySearch(matrix, srchVal, hi, lo)
+        else:
+            lo = mi + 1
+            # return matrixBinarySearch(matrix, srchVal, hi, lo)
+    return 'Value not found'
 
-    size = rows * cols
+def menu():
+    options = {'1 - Enter matrix' : enterMatrix,
+               '2 - Randomly generate matrix, specifying the values range limites': generateMatrix,
+               '3 - Exit' : exit}
 
-    i = len(matrix) // 2
-    j = len(matrix[0]) // 2
-
-    i_srch = 0
-    j_srch = 0
-
-    while found == False:
-        if srchVal >= matrix[i][0] and srchVal <= matrix[i][cols-1]:
-            i_srch = i
-            found = True
-        elif srchVal<=matrix[i][0]:
-            i //= 2 
-            if i == 0:
-                i_srch = 0
-                found = True
-        elif srchVal>=matrix[i][cols-1]:
-            i = ceil(i+i/2)
-            if i == rows-1:
-                i_srch = i
-                found = True
-    j_srch = arrayBinarySearch(matrix[i_srch], 0, cols-1, srchVal)
-    if j_srch == -1:
-        return "value not found"
-
-    return i_srch, j_srch
-
-
-def main():
-    print("Hello, you have 2 options to run the programm:\n\
-        option 1 - input matrix;\n\
-        option 2 - randomly generate matrix, specifying the values range limites\n\
-        if you want to to choose option 1, type '1'\n\
-        if you want to to choose option 2, type '2'\n\
-        to exit, just type 'exit'")
+    print("Hello :)\nYou are in Menu, choose one option to continue:")
 
     while True:
+        for key in options.keys():
+            print(key)
         option = input()
-        Exit(option)
-        if option == '1':
-            MatrixA = inputMatrix()
-            searchValue = int(input("Enter a value, you want to search: "))
-            Exit(searchValue)
-            resultIndices = MatrixbinarySearch(sortMatrix(MatrixA), searchValue)
-            break
-        elif option == '2':
-            MatrixA = generateMatrix()
-            searchValue = int(input("Enter a value, you want to search: "))
-            Exit(searchValue)
-            resultIndices = MatrixbinarySearch(sortMatrix(MatrixA), searchValue)
-            break
-        else:
-            print("you have only 2 options, try again: ")
-            continue
+        for i in options.keys():
+            if option == i[0]:
+                return options[i]()
+        print('Oops!\nTry to choose from available options:')
 
-    print("indeces of value, you search: ", resultIndices)
+def getIndecesOfSearchValue():
+    matrix = sortMatrix(menu())
+    searchValue = int(input("Enter a value, you want to search: "))
+    Exit(searchValue)
+    return matrixBinarySearch(matrix, searchValue)
 
 
-main()
+resultIndices = getIndecesOfSearchValue()
+print("Indeces in sorted matrix of value, you search: ", resultIndices)
+
