92ff4b10dfb0b8869771903642da9ad625ab0bc2
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2021-09-30 10:31:48 +0300

    Update pract2.py

--- pract2.py
+++ pract2.py
@@ -1,22 +1,25 @@
 import numpy as np
 from random import randrange
-from math import ceil
+
 
 def isNumber(N):
 	try:
 		int(N)
 		return True
 	except:
-		    return False
+		return False
+
 
 def isPositive(N):
   	return N >= 0
+
 
 def Exit(val):
     if val != 'exit':
         print("If you want to exit, type 'exit'")
     else:
         exit()
+
 
 def getPositiveNumber(m):
 # if m == True, function return positive number,
@@ -36,6 +39,7 @@ def getPositiveNumber(m):
         break
     return num
 
+
 def enterMatrix():
     print("Enter number of rows: ")
     N = getPositiveNumber(True)
@@ -44,7 +48,7 @@ def enterMatrix():
 
     matrix = np.zeros((N, M))
 
-    print("Use 'Enter' to input the matrix")
+    print("Please enter matrix element by element")
 
     for i in range(N):
         for j in range(M):
@@ -60,7 +64,7 @@ def generateMatrix():
     print("Enter number of columns: ")
     M = getPositiveNumber(True)
     matrix = np.zeros((N, M))  
-    print("Input range using 'Enter'")
+    print("Input range number by number")
     while True:
         a = getPositiveNumber(False)
         b = getPositiveNumber(False)
@@ -74,6 +78,7 @@ def generateMatrix():
     
     print("YOUR MATRIX:\n", matrix)
     return matrix
+
 
 def sortMatrix(matrix):
     operationsCounter = 0
@@ -90,10 +95,59 @@ def sortMatrix(matrix):
                 temp = matrix[j//col][j % col]
                 matrix[j//col][j % col] = matrix[(j + 1)//col][(j + 1)% col]
                 matrix[(j + 1)//col][(j + 1)% col] = temp
-                operationsCounter += 3
+                operationsCounter += 1
     print("matrix sort operationsCounter = ", operationsCounter)
     print("SORTED MATRIX:\n", matrix)
     return matrix
+
+
+"""
+def arrayBinarySearch (arr, l, r, x):
+    if r >= l:
+        mid = l + (r - l) // 2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return arrayBinarySearch(arr, l, mid-1, x)
+        else:
+            return arrayBinarySearch(arr, mid + 1, r, x)
+    else:
+        return -1
+
+def MatrixbinarySearch(matrix, srchVal):
+    found = False
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    i = len(matrix) // 2
+    j = len(matrix[0]) // 2
+
+    i_srch = 0
+    j_srch = 0
+
+    while found == False:
+        if srchVal >= matrix[i][0] and srchVal <= matrix[i][cols-1]:
+            i_srch = i
+            found = True
+        elif srchVal<=matrix[i][0]:
+            i //= 2 
+            if i == 0:
+                i_srch = 0
+                found = True
+        elif srchVal>=matrix[i][cols-1]:
+            i = ceil(i+i/2)
+            if i == rows-1:
+                i_srch = i
+                found = True
+    j_srch = arrayBinarySearch(matrix[i_srch], 0, cols-1, srchVal)
+    if j_srch == -1:
+        return "value not found"
+
+    return i_srch, j_srch
+"""
+
 
 def matrixBinarySearch(matrix, srchVal):
     cols = len(matrix[0])
@@ -114,6 +168,7 @@ def matrixBinarySearch(matrix, srchVal):
             # return matrixBinarySearch(matrix, srchVal, hi, lo)
     return 'Value not found'
 
+
 def menu():
     options = {'1 - Enter matrix' : enterMatrix,
                '2 - Randomly generate matrix, specifying the values range limites': generateMatrix,
@@ -130,13 +185,15 @@ def menu():
                 return options[i]()
         print('Oops!\nTry to choose from available options:')
 
+
 def getIndecesOfSearchValue():
     matrix = sortMatrix(menu())
-    searchValue = int(input("Enter a value, you want to search: "))
+    print("Enter a value, you want to search: ")
+    searchValue = getPositiveNumber(False)
     Exit(searchValue)
     return matrixBinarySearch(matrix, searchValue)
 
 
-resultIndices = getIndecesOfSearchValue()
-print("Indeces in sorted matrix of value, you search: ", resultIndices)
-
+while True:
+    resultIndices = getIndecesOfSearchValue()
+    print("Indeces in sorted matrix of value, you search: ", resultIndices)
